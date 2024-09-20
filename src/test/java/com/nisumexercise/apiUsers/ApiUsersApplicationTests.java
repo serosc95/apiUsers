92ba86cdb6cf3a333d82b063ec7d8ecc5d423e7f
Oscar De Leon <oscardleon95@gmail.com>
2024-09-20 13:22:40 -0500

    init project

---
+++ src/test/java/com/nisumexercise/apiUsers/ApiUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nisumexercise.apiUsers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
